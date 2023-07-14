a13166dff482a52dc83afe38acb824547bea7f4f
InfamousTechnician <48908633+InfamousTechnician@users.noreply.github.com>
2023-07-14 13:50:00 +0200

    Add files via upload

---
+++ Srcs/LFA_dev/stand_alone_16bitLFA/LFA32.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10.07.2023 12:03:09
+-- Design Name: 
+-- Module Name: LFA32 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LFA32 is
+    Port ( x : in STD_LOGIC_VECTOR (31 downto 0);
+           y : in STD_LOGIC_VECTOR (31 downto 0);
+           s : in STD_LOGIC_VECTOR (31 downto 0));
+end LFA32;
+
+architecture Behavioral of LFA32 is
+
+begin
+
+
+end Behavioral;
